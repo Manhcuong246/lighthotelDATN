e3e2c5662d1e1ccbe78564dc1c4079316e95ab00
Nguyen Bao <baongph50309@gmail.com>
2026-04-12 03:14:48 +0700

    feat: define basic fillable fields for BookingGuest

--- app/Models/BookingGuest.php
+++ app/Models/BookingGuest.php
@@ -4,4 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class BookingGuest extends Model{}
+class BookingGuest extends Model{
+    protected $fillable = [
+    'booking_id',
+    'name',
+];
+}
